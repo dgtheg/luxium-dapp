e4f37083aeeace69fba030cef71a406fa580ce72
Daniel Gagnidze <danielgagnidze@Daniels-MacBook-Pro.local>
2025-04-27 12:35:26 +0200

    Build for production

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/luxium-dapp/', // 👈 THIS is what tells Vite where your app is hosted
+  base: '/luxium-dapp/',   // <--- THIS IS THE MOST IMPORTANT PART
   plugins: [react()],
 });
